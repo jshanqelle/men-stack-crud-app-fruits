bc672be5cc9cdd4f376b756fc43bdbdbdaff85a7
jshanqelle <johnsonshanqelle@gmail.com>
2025-02-14 11:55:38 -0500

    create landing page for app

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
-// Here is where we import modules
-// We begin by loading Express
 const express = require('express');
 
 const app = express();
+
+// GET /
+app.get("/", async (req, res) => {
+    res.send('index.ejs');
+  });
 
 app.listen(3000, () => {
   console.log('Listening on port 3000');
