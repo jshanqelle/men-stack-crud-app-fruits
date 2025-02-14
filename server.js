027060089477bf241f4ed85b4b68e29c3d085cc8
jshanqelle <johnsonshanqelle@gmail.com>
2025-02-14 13:09:13 -0500

    intergarte fruit model into server

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const dotenv = require('dotenv');
 dotenv.config();
+
 const express = require('express');
 const mongoose = require('mongoose');
+
 const app = express();
 
 mongoose.connect(process.env.MONGODB_URI);
@@ -9,6 +11,10 @@ mongoose.connect(process.env.MONGODB_URI);
 mongoose.connection.on('connected', () => {
     console.log(`Connected on MongoDB ${mongoose.connection.name}`)
 });
+
+const Fruit = require('./models/fruit.js');
+
+
 
 // GET /
 app.get("/", async (req, res) => {
